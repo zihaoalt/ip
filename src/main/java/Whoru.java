9098ead3ec97a41efbb6696e6ca3b26511f88a44
zihaoalt <xuzihao024572@gmail.com>
2026-01-23 13:31:05 +0800

    Complete level 0 tasks
    Rename Greet Exit

---
+++ src/main/java/Whoru.java
@@ -0,0 +1,14 @@
+public class Whoru {
+    public static void main(String[] args) {
+        String logo =
+                "____________________________________________________________\n"
+                        + "Hello! I'm [Whoru]\n"
+                        + "What can I do for you?\n"
+                        + "____________________________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________\n";
+
+        System.out.println(logo);
+    }
+}
+
