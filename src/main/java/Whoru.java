eb15cf330f4ac25ceb321f2e4d6595a8f8996f51
zihaoalt <xuzihao024572@gmail.com>
2026-01-30 11:51:09 +0800

    Finish level 1 echo task

--- src/main/java/Whoru.java
+++ src/main/java/Whoru.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
+
 public class Whoru {
     public static void main(String[] args) {
         String logo =
                 "____________________________________________________________\n"
-                        + "Hello! I'm [Whoru]\n"
+                        + "Hello! I'm Whoru\n"
                         + "What can I do for you?\n"
-                        + "____________________________________________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
+                        + "Try typing something\n"
                         + "____________________________________________________________\n";
 
+
         System.out.println(logo);
+
+        new Whoru().listenCommand();
+    }
+
+    private void listenCommand() {
+        Scanner in = new Scanner(System.in);
+        String line;
+        String outString;
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                outString = "____________________________________________________________\n"
+                                + "Byebye hope to see you next time\n"
+                                + "____________________________________________________________\n";
+                System.out.println(outString);
+                break;
+            }
+            outString = "____________________________________________________________\n"
+                            + "Did you just say " + line + "\n"
+                            + "____________________________________________________________\n";
+            System.out.println(outString);
+        }
     }
 }
 
